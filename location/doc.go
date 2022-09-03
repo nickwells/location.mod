93bbab3f41b6e3bde253650abf6bac67e7be0e0b
Nick Wells <wells.nicholas@gmail.com>
2022-09-03 10:35:04 +0100

    refactor: minor changes to keep gofumpt quiet

--- location/doc.go
+++ location/doc.go
@@ -1,5 +1,4 @@
 /*
-
 Package location provides a standard way of recording an indexed location in
 some source. For instance a line in a file.
 
@@ -8,6 +7,5 @@ index can be incremented. The content of the current location can be added
 along with any notes. It is also possible to construct chains of locations to
 reflect a series of file include directives. This chain can be checked to
 ensure there are no loops.
-
 */
 package location
